abc35d8463aa8927c96f1255ff088ae8f722f4f0
muhammad Anas <134830845+muhammadanasdevs@users.noreply.github.com>
2023-05-28 14:04:43 +0500

    Update Migrator.php

--- src/Migrations/Migrator.php
+++ src/Migrations/Migrator.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Nwidart\Modules\Migrations;
+namespace Muhammadanasdevs\Modules\Migrations;
 
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
-use Nwidart\Modules\Module;
-use Nwidart\Modules\Support\Config\GenerateConfigReader;
+use Muhammadanasdevs\Modules\Module;
+use Muhammadanasdevs\Modules\Support\Config\GenerateConfigReader;
 
 class Migrator
 {
