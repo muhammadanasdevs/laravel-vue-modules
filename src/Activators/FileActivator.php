64f85dbb4e07f1939818dd9205e9697b30e7d287
muhammad Anas <134830845+muhammadanasdevs@users.noreply.github.com>
2023-05-28 14:15:52 +0500

    Update FileActivator.php

--- src/Activators/FileActivator.php
+++ src/Activators/FileActivator.php
@@ -1,14 +1,14 @@
 <?php
 
-namespace Nwidart\Modules\Activators;
+namespace Muhammadanasdevs\Modules\Activators;
 
 use Illuminate\Cache\CacheManager;
 use Illuminate\Config\Repository as Config;
 use Illuminate\Container\Container;
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Filesystem\Filesystem;
-use Nwidart\Modules\Contracts\ActivatorInterface;
-use Nwidart\Modules\Module;
+use Muhammadanasdevs\Modules\Contracts\ActivatorInterface;
+use Muhammadanasdevs\Modules\Module;
 
 class FileActivator implements ActivatorInterface
 {
