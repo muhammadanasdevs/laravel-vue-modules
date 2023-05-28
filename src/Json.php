91771a9c181e68cb63f0e75e202ccf64455aa70d
muhammad Anas <134830845+muhammadanasdevs@users.noreply.github.com>
2023-05-28 13:58:40 +0500

    Update Json.php

--- src/Json.php
+++ src/Json.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace Nwidart\Modules;
+namespace Muhammadanasdevs\Modules;
 
 use Illuminate\Filesystem\Filesystem;
-use Nwidart\Modules\Exceptions\InvalidJsonException;
+use Muhammadanasdevs\Modules\Exceptions\InvalidJsonException;
 
 class Json
 {
