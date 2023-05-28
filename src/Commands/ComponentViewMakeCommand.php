f5006f80a300645fcbc925c1d11a74c1b6c035b0
muhammad Anas <134830845+muhammadanasdevs@users.noreply.github.com>
2023-05-28 14:50:03 +0500

    Update ComponentViewMakeCommand.php

--- src/Commands/ComponentViewMakeCommand.php
+++ src/Commands/ComponentViewMakeCommand.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Nwidart\Modules\Commands;
+namespace Muhammadanasdevs\Modules\Commands;
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Str;
-use Nwidart\Modules\Support\Config\GenerateConfigReader;
-use Nwidart\Modules\Support\Stub;
-use Nwidart\Modules\Traits\ModuleCommandTrait;
+use Muhammadanasdevs\Modules\Support\Config\GenerateConfigReader;
+use Muhammadanasdevs\Modules\Support\Stub;
+use Muhammadanasdevs\Modules\Traits\ModuleCommandTrait;
 use Symfony\Component\Console\Input\InputArgument;
 
 class ComponentViewMakeCommand extends GeneratorCommand
