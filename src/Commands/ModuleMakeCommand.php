58209bdecf496f34f0339cdf0441ebe3bddcfde4
muhammad Anas <134830845+muhammadanasdevs@users.noreply.github.com>
2023-05-28 14:41:16 +0500

    Update ModuleMakeCommand.php

--- src/Commands/ModuleMakeCommand.php
+++ src/Commands/ModuleMakeCommand.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Nwidart\Modules\Commands;
+namespace Muhammadanasdevs\Modules\Commands;
 
 use Illuminate\Console\Command;
-use Nwidart\Modules\Contracts\ActivatorInterface;
-use Nwidart\Modules\Generators\ModuleGenerator;
+use Muhammadanasdevs\Modules\Contracts\ActivatorInterface;
+use Muhammadanasdevs\Modules\Generators\ModuleGenerator;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 
