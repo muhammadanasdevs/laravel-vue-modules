4ba04fea02a1e2747ac441b14bd83c3ca57e3a2f
muhammad Anas <134830845+muhammadanasdevs@users.noreply.github.com>
2023-05-28 14:44:18 +0500

    Update MigrateRefreshCommand.php

--- src/Commands/MigrateRefreshCommand.php
+++ src/Commands/MigrateRefreshCommand.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace Nwidart\Modules\Commands;
+namespace Muhammadanasdevs\Modules\Commands;
 
 use Illuminate\Console\Command;
-use Nwidart\Modules\Traits\ModuleCommandTrait;
+use Muhammadanasdevs\Modules\Traits\ModuleCommandTrait;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 
