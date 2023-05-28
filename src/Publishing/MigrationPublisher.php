44f5035a79a2f3ff67fc08997b1d8f94b3e404c7
muhammad Anas <134830845+muhammadanasdevs@users.noreply.github.com>
2023-05-28 14:02:17 +0500

    Update MigrationPublisher.php

--- src/Publishing/MigrationPublisher.php
+++ src/Publishing/MigrationPublisher.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace Nwidart\Modules\Publishing;
+namespace Muhammadanasdevs\Modules\Publishing;
 
-use Nwidart\Modules\Migrations\Migrator;
+use Muhammadanasdevs\Modules\Migrations\Migrator;
 
 class MigrationPublisher extends AssetPublisher
 {
