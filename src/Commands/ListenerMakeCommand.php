3dad29e4ccf575dbb225f0570ec39d88cb43c2e8
muhammad Anas <134830845+muhammadanasdevs@users.noreply.github.com>
2023-05-28 14:45:54 +0500

    Update ListenerMakeCommand.php

--- src/Commands/ListenerMakeCommand.php
+++ src/Commands/ListenerMakeCommand.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Nwidart\Modules\Commands;
+namespace Muhammadanasdevs\Modules\Commands;
 
 use Illuminate\Support\Str;
-use Nwidart\Modules\Module;
-use Nwidart\Modules\Support\Config\GenerateConfigReader;
-use Nwidart\Modules\Support\Stub;
-use Nwidart\Modules\Traits\ModuleCommandTrait;
+use Muhammadanasdevs\Modules\Module;
+use Muhammadanasdevs\Modules\Support\Config\GenerateConfigReader;
+use Muhammadanasdevs\Modules\Support\Stub;
+use Muhammadanasdevs\Modules\Traits\ModuleCommandTrait;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 
